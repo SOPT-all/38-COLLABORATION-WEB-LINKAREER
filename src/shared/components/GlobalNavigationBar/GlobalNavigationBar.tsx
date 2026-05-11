7c9a2541dd6ba314ab46b5bb5a107a549f633349
jiwonsudo <wondev112@gmail.com>
2026-05-12 01:42:45 +0900

    feat: GNB 레이아웃 완성

---
+++ src/shared/components/GlobalNavigationBar/GlobalNavigationBar.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router';
+
+import IcBell from '@assets/svg/IcBell';
+import IcBookMark from '@assets/svg/IcBookmark';
+import IcMenu2 from '@assets/svg/IcMenu2';
+import Logo from '@assets/svg/Logo';
+
+import * as styles from './globalNavigationBar.css';
+
+const ACTION_ITEMS = [
+  { label: '알림', Icon: IcBell },
+  { label: '북마크', Icon: IcBookMark },
+  { label: '메뉴', Icon: IcMenu2 },
+] as const;
+
+const GlobalNavigationBar = () => {
+  return (
+    <>
+      <header className={styles.container}>
+        <section className={styles.frame}>
+          <Link to="/" className={styles.logo} aria-label="Linkareer home">
+            <Logo className={styles.logoIcon} />
+          </Link>
+
+          <menu className={styles.content}>
+            {ACTION_ITEMS.map(({ label, Icon }) => (
+              <li key={label}>
+                <button
+                  className={styles.iconButton}
+                  type="button"
+                  aria-label={label}
+                >
+                  <Icon className={styles.icon} aria-hidden={true} />
+                </button>
+              </li>
+            ))}
+          </menu>
+        </section>
+      </header>
+    </>
+  );
+};
+
+export default GlobalNavigationBar;
---
+++ src/shared/components/GlobalNavigationBar/globalNavigationBar.css.ts
@@ -0,0 +1,60 @@
+import { style } from '@vanilla-extract/css';
+
+import { vars } from '@styles/theme.css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-end',
+  gap: '2rem',
+  backgroundColor: vars.color.blue500,
+  padding: '0 0.8rem 0 1.6rem',
+  width: '100%', // = 375px
+  height: '6rem',
+});
+
+export const frame = style({
+  display: 'flex',
+  flex: '1 0 0',
+  alignItems: 'center',
+  alignSelf: 'stretch',
+  justifyContent: 'space-between',
+});
+
+export const logo = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '12rem',
+  height: '4.4rem',
+});
+
+export const logoIcon = style({
+  flexShrink: 0,
+  width: '100%',
+  height: '100%',
+});
+
+export const content = style({
+  display: 'flex',
+  alignItems: 'center',
+  height: '4.4rem',
+  listStyle: 'none',
+});
+
+export const iconButton = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: '1.2rem 1rem',
+  width: '4rem',
+  height: '4.4rem',
+});
+
+export const icon = style({
+  flexShrink: '0',
+  width: '2rem',
+  height: '2rem',
+});
