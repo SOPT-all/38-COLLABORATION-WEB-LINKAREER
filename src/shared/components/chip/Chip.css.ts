251879c13bbaf54900d1c788d2e5bf91d0cdfef5
Soy <da5868@naver.com>
2026-05-12 22:08:01 +0900

    feat: chip 컴포넌트 구현

---
+++ src/shared/components/chip/Chip.css.ts
@@ -0,0 +1,50 @@
+import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
+
+import { color } from '@styles/tokens/color';
+import { typography } from '@styles/tokens/typography';
+
+export const chip = recipe({
+  base: {
+    display: 'inline-flex',
+    alignItems: 'center',
+    gap: '0.2rem',
+    outline: 'none',
+    border: '1px solid transparent',
+    ...typography.caption_m_12,
+    borderRadius: '20rem',
+    padding: '0.8rem 1.4rem',
+    height: '3.4rem',
+    whiteSpace: 'nowrap',
+  },
+  variants: {
+    variant: {
+      default: {
+        border: `1px solid ${color.gray200}`,
+        color: color.gray700,
+      },
+      filled: {
+        backgroundColor: color.blue500,
+        color: color.white,
+      },
+      outline: {
+        border: `1px solid ${color.blue500}`,
+        color: color.blue500,
+      },
+      outlineFilled: {
+        border: `1px solid ${color.blue500}`,
+        backgroundColor: color.blue50,
+        color: color.blue500,
+      },
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+
+export const text = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
---
+++ src/shared/components/chip/Chip.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+import SvgIcChevronDownGray700 from '@assets/svg/IcChevronDownGray700';
+
+import * as styles from './Chip.css';
+
+export type ChipVariant = 'default' | 'filled' | 'outline' | 'outlineFilled';
+
+export interface ChipProps extends Omit<
+  React.ComponentProps<'button'>,
+  'children' | 'width'
+> {
+  children: React.ReactNode;
+  variant?: ChipVariant;
+  rightIcon?: boolean;
+}
+
+const ActionButton = ({
+  children,
+  variant = 'default',
+  rightIcon = false,
+  type = 'button',
+
+  ...props
+}: ChipProps) => {
+  return (
+    <button type={type} className={styles.chip({ variant })} {...props}>
+      <span className={styles.text}>{children}</span>
+      {rightIcon && <SvgIcChevronDownGray700 width="1.8rem" height="1.8rem" />}
+    </button>
+  );
+};
+
+export default ActionButton;
