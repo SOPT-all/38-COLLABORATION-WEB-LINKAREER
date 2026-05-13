cc2b41aa5c8a385196368a86b7ed9ed02c4b2b88
jjangminii <wjdals030328@catholic.ac.kr>
2026-05-14 01:15:10 +0900

    chore: 테스트코드 제거

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,28 +1,8 @@
-import { useState } from 'react';
-
-import Indicator from '@pages/recruitDetail/components/indicator/Indicator';
-
-const TOTAL_COUNT = 4;
-
 const HomePage = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handlePrev = () => setCurrentIndex((prev) => Math.max(prev - 1, 0));
-  const handleNext = () =>
-    setCurrentIndex((prev) => Math.min(prev + 1, TOTAL_COUNT - 1));
-
   return (
     <>
-      <h1></h1>
-      <Indicator totalCount={TOTAL_COUNT} currentIndex={currentIndex} />
-      <button onClick={handlePrev} disabled={currentIndex === 0}>
-        이전
-      </button>
-      <button onClick={handleNext} disabled={currentIndex === TOTAL_COUNT - 1}>
-        다음
-      </button>
+      <h1>홈페이지</h1>
     </>
   );
 };
-
 export default HomePage;
