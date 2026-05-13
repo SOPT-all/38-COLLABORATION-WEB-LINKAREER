994ea82f1852fee79a8b52eb32a0e26ec8d555a9
jjangminii <wjdals030328@catholic.ac.kr>
2026-05-14 02:04:35 +0900

    refactor: 애니메이션 관련 코드 제거 및 스타일 개선

--- src/pages/recruitDetail/components/indicator/Indicator.css.ts
+++ src/pages/recruitDetail/components/indicator/Indicator.css.ts
@@ -1,29 +1,7 @@
-import { keyframes, style } from '@vanilla-extract/css';
+import { style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
 import { vars } from '@styles/theme.css';
-
-const expandAnimation = keyframes({
-  from: {
-    opacity: 0.5,
-    width: '0.7rem',
-  },
-  to: {
-    opacity: 1,
-    width: '1.6rem',
-  },
-});
-
-const collapseAnimation = keyframes({
-  from: {
-    opacity: 1,
-    width: '1.6rem',
-  },
-  to: {
-    opacity: 0.5,
-    width: '0.7rem',
-  },
-});
 
 export const wrapper = style({
   display: 'flex',
@@ -35,19 +13,20 @@ export const dotRecipe = recipe({
   base: {
     borderRadius: '9999px',
     height: '0.7rem',
+    transition: 'width 0.5s ease, opacity 0.5s ease',
   },
 
   variants: {
     active: {
       true: {
-        background: vars.color.blue500,
+        opacity: 1,
+        backgroundColor: vars.color.blue500,
         width: '1.6rem',
-        animation: `${expandAnimation} 0.5s ease forwards`,
       },
       false: {
-        background: vars.color.gray200,
+        opacity: 0.5,
+        backgroundColor: vars.color.gray200,
         width: '0.7rem',
-        animation: `${collapseAnimation} 0.5s ease forwards`,
       },
     },
   },
