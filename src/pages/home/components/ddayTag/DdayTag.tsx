61a32342bbc1b6e0c901805e36fdceb2f6625899
jiwonsudo <wondev112@gmail.com>
2026-05-13 23:38:07 +0900

    feat: Ddaytag 구현

---
+++ src/pages/home/components/ddayTag/DdayTag.tsx
@@ -0,0 +1,12 @@
+import * as style from './ddayTag.css';
+
+interface DdayTagProps {
+  daysLeft: number;
+  size?: 'small' | 'medium';
+}
+
+const DdayTag = ({ size = 'small', daysLeft }: DdayTagProps) => {
+  return <span className={style.ddayTag({ size })}>D-{daysLeft}</span>;
+};
+
+export default DdayTag;
