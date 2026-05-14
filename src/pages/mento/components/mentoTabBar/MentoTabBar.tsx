85ed02daf2167b34f030e2897d8fa9fa54de3ff7
Jina <jinatye@naver.com>
2026-05-14 20:43:34 +0900

    feat: mentoTabBar 구현

---
+++ src/pages/mento/components/mentoTabBar/MentoTabBar.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+import Chip from '@components/chip/Chip';
+import { MENTO_COMPANY_CHIPS } from '@pages/mento/constants/mentoChips';
+
+import * as styles from './MentoTabBar.css';
+
+const MentoTabBar = () => {
+  const [selectedChip, setSelectedChip] = useState<string>();
+  return (
+    <div className={styles.container}>
+      {MENTO_COMPANY_CHIPS.map((chip) => (
+        <Chip
+          onClick={() => setSelectedChip(chip)}
+          key={chip}
+          variant={selectedChip === chip ? 'filled' : 'default'}
+        >
+          {chip}
+        </Chip>
+      ))}
+    </div>
+  );
+};
+
+export default MentoTabBar;
---
+++ src/pages/mento/constants/mentoChips.ts
@@ -0,0 +1,1 @@
+export const MENTO_COMPANY_CHIPS = ['삼성', 'SK', '현대', 'LG', '공기업'];
