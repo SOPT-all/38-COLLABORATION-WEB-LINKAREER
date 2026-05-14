08e807af84798fb92f6dbe6d8d463ff26a8b14c0
Jina <jinatye@naver.com>
2026-05-14 17:05:50 +0900

    feat: 멘토페이지 검색바 구현

---
+++ src/pages/mento/components/mentoSearchBar/MentoSearchBar.tsx
@@ -0,0 +1,15 @@
+import * as styles from './MentoSearchBar.css';
+
+const MentoSearchBar = () => {
+  return (
+    <div className={styles.container}>
+      <input
+        className={styles.inputField}
+        placeholder="삼성, 마케팅, 홍익대학교..."
+      />
+      <button className={styles.button}>검색</button>
+    </div>
+  );
+};
+
+export default MentoSearchBar;
