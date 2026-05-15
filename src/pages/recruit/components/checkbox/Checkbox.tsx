d4d14926400dacf3c6a9f7e87af5d05997231b13
Soy <da5868@naver.com>
2026-05-15 16:30:48 +0900

    feat: checkbox 컴포넌트 구현

---
+++ src/pages/recruit/components/checkbox/Checkbox.tsx
@@ -0,0 +1,27 @@
+import SvgIcCheckWhite from '@assets/svg/IcCheckWhite';
+
+import * as styles from './Checkbox.css';
+
+interface CheckboxProps {
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+}
+
+const Checkbox = ({ checked, onChange }: CheckboxProps) => {
+  return (
+    <label htmlFor="checkbox" className={styles.container}>
+      <input
+        id="checkbox"
+        type="checkbox"
+        className={styles.hiddenInput}
+        checked={checked}
+        onChange={(e) => onChange(e.target.checked)}
+      />
+      <div className={styles.checkbox}>
+        {checked && <SvgIcCheckWhite width={'1.6rem'} height={'1.6rem'} />}
+      </div>
+    </label>
+  );
+};
+
+export default Checkbox;
