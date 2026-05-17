6f34aa58ee2b402636144ad47cefafdf05733ab1
Jina <jinatye@naver.com>
2026-05-17 21:23:48 +0900

    feat: isPressed에 따른 스타일 분기

--- src/pages/mento/components/writeButton/WriteButton.css.ts
+++ src/pages/mento/components/writeButton/WriteButton.css.ts
@@ -1,13 +1,22 @@
-import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 
 import { vars } from '@styles/theme.css';
 
-export const writeButton = style({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  borderRadius: '50%',
-  backgroundColor: vars.color.blue500,
-  width: '4.8rem',
-  height: '4.8rem',
+export const writeButton = recipe({
+  base: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '50%',
+    backgroundColor: vars.color.blue500,
+    width: '4.8rem',
+    height: '4.8rem',
+  },
+  variants: {
+    isPressed: {
+      true: {
+        backgroundColor: vars.color.blue600,
+      },
+    },
+  },
 });
--- src/pages/mento/components/writeButton/WriteButton.tsx
+++ src/pages/mento/components/writeButton/WriteButton.tsx
@@ -2,9 +2,16 @@ import SvgIcWrite from '@assets/svg/IcWrite';
 
 import * as styles from './WriteButton.css';
 
-const WriteButton = () => {
+interface WriteButtonProps {
+  isPressed?: boolean;
+}
+const WriteButton = ({ isPressed = false }: WriteButtonProps) => {
   return (
-    <button type="button" className={styles.writeButton} aria-label="글쓰기">
+    <button
+      type="button"
+      className={styles.writeButton({ isPressed })}
+      aria-label="글쓰기"
+    >
       <SvgIcWrite width="2.8rem" height="2.8rem" />
     </button>
   );
