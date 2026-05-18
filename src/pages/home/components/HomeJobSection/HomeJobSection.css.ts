4efce51d472c6cc9a098daca5c263fadb8ed8ca0
yumin-kim4757 <rladbals0211@gmail.com>
2026-05-19 02:32:58 +0900

    feat: 홈 채용공고 섹션 구현

---
+++ src/pages/home/components/HomeJobSection/HomeJobSection.css.ts
@@ -0,0 +1,17 @@
+import { style } from '@vanilla-extract/css';
+
+export const mainContainer = style({
+  display: 'flex',
+  paddingLeft: '1.6rem',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '1.6rem',
+  alignSelf: 'stretch',
+});
+
+export const activityList = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.6rem',
+  alignSelf: 'stretch',
+});
