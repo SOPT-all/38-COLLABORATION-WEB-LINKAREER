0c4550a1bb85b345ec07324461079627cfebaeb9
Soy <da5868@naver.com>
2026-05-19 03:02:36 +0900

    fix: 공고 개수 결과 로직 수정

--- src/pages/recruit/utils/resultNumber.ts
+++ src/pages/recruit/utils/resultNumber.ts
@@ -1,30 +1,48 @@
 import {
   BUSINESS_JOB,
+  type JobCategory,
   MARKETING_JOB,
   SALES_JOB,
 } from '@pages/recruit/constants/filterOptions';
 import { CONTENT_RESULT } from '@pages/recruit/constants/resultNumber';
 import type { FilterValues } from '@pages/recruit/types/filter';
 
+const getJobResultCount = (selectedJob: JobCategory): number => {
+  switch (selectedJob) {
+    case MARKETING_JOB:
+      return CONTENT_RESULT.marketing;
+    case SALES_JOB:
+      return CONTENT_RESULT.sales;
+    case BUSINESS_JOB:
+      return CONTENT_RESULT.business;
+    default:
+      return 0;
+  }
+};
+
+// 직무 제외 카테고리에서 하나라도 선택된 경우
+const hasAndFilters = (selectedFilters: FilterValues) => {
+  return (
+    selectedFilters.companyTypes.length > 0 ||
+    selectedFilters.employmentTypes.length > 0 ||
+    selectedFilters.regions.length > 0
+  );
+};
+
 // 선택된 필터에 따른 결과 개수 계산
-export const getResultCount = (selectedFilters: FilterValues): string => {
+export const getResultCount = (
+  selectedFilters: FilterValues,
+  dataCount?: number, // 실제 데이터 개수
+): string => {
   const { jobCategories } = selectedFilters;
+
+  if (hasAndFilters(selectedFilters) && dataCount != null) {
+    return dataCount.toLocaleString();
+  }
 
   if (jobCategories.length === 0)
     return CONTENT_RESULT.default.toLocaleString();
 
-  const total = jobCategories.reduce((acc, cur) => {
-    const count =
-      cur === MARKETING_JOB
-        ? CONTENT_RESULT.marketing
-        : cur === SALES_JOB
-          ? CONTENT_RESULT.sales
-          : cur === BUSINESS_JOB
-            ? CONTENT_RESULT.business
-            : 0;
-
-    return acc + count;
-  }, 0);
-
-  return total.toLocaleString();
+  const selectedJob = jobCategories[0];
+  return getJobResultCount(selectedJob).toLocaleString();
 };
