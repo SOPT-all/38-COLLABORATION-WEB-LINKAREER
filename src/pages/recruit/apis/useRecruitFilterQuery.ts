f47051583fdc45da776a79c6c8824ab020828358
Soy <da5868@naver.com>
2026-05-19 01:29:37 +0900

    feat: api 호출 쿼리 작성

---
+++ src/pages/recruit/apis/useRecruitFilterQuery.ts
@@ -0,0 +1,45 @@
+import { API_ENDPOINT } from '@constants/apiEndpoints';
+import { QUERY_KEY } from '@constants/queryKey';
+import { useQuery } from '@tanstack/react-query';
+
+import type { RecruitmentResponse } from '@apis/__generated__/data-contracts';
+import { HTTPMethod, request, type RequestConfig } from '@apis/config/request';
+import type { FilterValues } from '@pages/recruit/types/filter';
+
+const buildRecruitQuery = (
+  filters?: FilterValues,
+): RequestConfig['query'] | undefined => {
+  if (!filters) return undefined;
+
+  const query = {
+    ...(filters.jobCategories.length > 0 && {
+      jobCategories: filters.jobCategories,
+    }),
+    ...(filters.companyTypes.length > 0 && {
+      companyTypes: filters.companyTypes,
+    }),
+    ...(filters.employmentTypes.length > 0 && {
+      employmentTypes: filters.employmentTypes,
+    }),
+    ...(filters.regions.length > 0 && {
+      regions: filters.regions,
+    }),
+  };
+
+  return Object.keys(query).length > 0 ? query : undefined;
+};
+
+export const getRecruitData = (filters?: FilterValues) => {
+  return request<RecruitmentResponse[]>({
+    method: HTTPMethod.GET,
+    url: API_ENDPOINT.RECRUIT.LIST,
+    query: buildRecruitQuery(filters),
+  });
+};
+
+export const useGetRecruitQuery = (filters?: FilterValues) => {
+  return useQuery({
+    queryKey: [...QUERY_KEY.RECRUIT, filters],
+    queryFn: () => getRecruitData(filters),
+  });
+};
