e1e3547d6669fbff63922b3733390e066cf1483c
jiwonsudo <wondev112@gmail.com>
2026-05-19 20:45:03 +0900

    feat: 합격 자료 조회 API 함수 추가

---
+++ src/pages/recruitDetail/apis/recruitDetailApi.ts
@@ -0,0 +1,34 @@
+import { API_ENDPOINT } from '@constants/apiEndpoints';
+import { QUERY_KEY } from '@constants/queryKey';
+import { useQuery } from '@tanstack/react-query';
+
+import type { PassCoverLetterResponse } from '@apis/__generated__/data-contracts';
+import { HTTPMethod, request } from '@apis/config/request';
+
+export const getPassCoverLetters = () => {
+  return request<PassCoverLetterResponse[]>({
+    method: HTTPMethod.GET,
+    url: API_ENDPOINT.RECRUIT.PASS_COVER_LETTERS,
+  });
+};
+
+export const usePassCoverLettersQuery = () => {
+  return useQuery({
+    queryKey: [...QUERY_KEY.RECRUIT_DETAIL, 'pass-cover-letters'],
+    queryFn: getPassCoverLetters,
+  });
+};
+
+export const getPassReviews = () => {
+  return request<PassCoverLetterResponse[]>({
+    method: HTTPMethod.GET,
+    url: API_ENDPOINT.RECRUIT.PASS_REVIEWS,
+  });
+};
+
+export const usePassReviewsQuery = () => {
+  return useQuery({
+    queryKey: [...QUERY_KEY.RECRUIT_DETAIL, 'pass-review'],
+    queryFn: getPassReviews,
+  });
+};
